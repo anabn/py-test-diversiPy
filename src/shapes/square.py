a22e2d22001bc6689afcb2f7c9586d1514c54a15
Anna Fetsun <anna.fetsun@avid.com>
2024-11-24 17:40:17 +0200

    use another way of initialise

--- src/shapes/square.py
+++ src/shapes/square.py
@@ -4,13 +4,18 @@ from .rectangle import Rectangle
 
 class Square(Rectangle):
     
-    def __init__(self, side: any):
+    def __init__(self):
+        pass
+    
+    def initialise_shape(self, shape_data_str: str) -> "Square":
+        shape_data = shape_data_str.split()
+        side = shape_data[self.get_param_index_value(shape_data, "Side") + 1]
         default_top_right = (0, 0)
         default_top_left = (side, side)
         super().__init__(default_top_right, default_top_left)
         Shape.__init__(self, ShapeType.SQUARE.value)
-        if side < 0:
+        if float(side) < 0:
             raise ValueError(f"{side} is negative")
-    
-    
+        
+        return self
     
