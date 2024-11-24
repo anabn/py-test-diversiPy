651a0451c952dc5b9e1b41963d2834c692eba0ae
Anna Fetsun <anna.fetsun@avid.com>
2024-11-24 17:04:51 +0200

    improve calling base class

--- src/shapes/square.py
+++ src/shapes/square.py
@@ -1,4 +1,5 @@
 from program.shapeType import ShapeType
+from program.shape import Shape
 from .rectangle import Rectangle
 
 class Square(Rectangle):
@@ -7,7 +8,7 @@ class Square(Rectangle):
         default_top_right = (0, 0)
         default_top_left = (side, side)
         super().__init__(default_top_right, default_top_left)
-        self.name = ShapeType.SQUARE.value
+        Shape.__init__(self, ShapeType.SQUARE.value)
         if side < 0:
             raise ValueError(f"{side} is negative")
     
